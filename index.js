c3128b31dfc7411c61fdb4b2a42e282fae5bcfea
MaximKireev <kyreyev.maksym@gmail.com>
2022-06-15 18:57:37 +0200

    almost all finished

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log('Hello world')
+const students = [
+	{
+		name: "Martin",
+		lastName: "Lant",
+		marks: [9, 8, null, 7, 5],
+	},
+
+	{
+		name: "Francesco",
+		lastName: "Portus",
+		marks: [5, 4, 2, 3, 2],
+	},
+
+	{
+		name: "Bill",
+		lastName: "Merdoc",
+		marks: [10, null, null, null, 10],
+	},
+
+	{
+		name: "John",
+		lastName: "Entman",
+		marks: [9, 8, 9, 7, 5],
+	},
+];
+
+const cardsContainer = document.querySelector(".cards-container");
+const returnVal = (obj) => {
+	return Math.round(
+		obj.reduce(function (previousValue, currentValue) {
+			return previousValue + currentValue;
+		}, 0) / obj.length
+	);
+};
+
+students.map((item) =>
+	cardsContainer.insertAdjacentHTML(
+		"beforeend",
+		`<div class=${
+			returnVal(item.marks) < 5 ? "card red-border" : "card green-border"
+		}>
+<p class="name">Name: ${item.name}</p>
+<p class="surname">Surname: ${item.lastName}</p>
+<div class="lectures">Lectures: ${
+			item.marks.filter((item) => item).length
+		}</div>
+<div class="ave-mark">Ave-mark: ${Math.round(
+			item.marks.reduce(function (previousValue, currentValue) {
+				return previousValue + currentValue;
+			}, 0) / item.marks.length
+		)}</div></div>`
+	)
+);
+
+const cards = document.querySelectorAll(".card");
+
+for (card of cards) {
+    let counter = 0
+	card.addEventListener("click", (e) => {
+        counter++
+		e.target.classList.toggle("gray-bg");
+	});
+
+
+}
